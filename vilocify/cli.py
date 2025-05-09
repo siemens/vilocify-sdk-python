siemens/vilocify-sdk-python

commit 1bd9485b77ead67a0ad0638f980c0f7eb789d4e4

Improve HTTP error handling
If available, include the error details in a custom error class.

Print errors better formatted on the CLI instead of crashing the CLI
with an exception.

Signed-off-by: Wolfgang Popp <popp.wolfgang@siemens.com>

vilocify/cli.py
@@ -6,6 +6,7 @@
 import io
 import json
 import logging
+import sys
 import textwrap
 from datetime import UTC, datetime, timedelta
 
@@ -13,7 +14,7 @@ import click
 from cyclonedx.model.bom import Bom
 from cyclonedx.model.bom import Component as BomComponent
 
-import vilocify
+from vilocify.http import JSONAPIRequestError, RequestError
 from vilocify.models import (
     Component,
     ComponentRequest,
@@ -36,9 +37,8 @@ MIT License
 
 @click.group()
 @click.option("--log-level", type=click.Choice(["DEBUG", "INFO", "ERROR"]), default="INFO")
-@click.version_option(version=vilocify.__version__, message=version_text)
 def cli(log_level: str):
-    logging.basicConfig(level=log_level)
+    logging.basicConfig(format="%(levelname)s: %(message)s", level=log_level)
 
 
 @cli.command()
@@ -171,5 +171,22 @@ def component_request(state: str):
         print()
 
 
+def main():
+    try:
+        exit_code = cli.main(standalone_mode=False)
+        sys.exit(exit_code)
+    except JSONAPIRequestError as e:
+        logging.error("%s - %s", e.error_code, e.message)
+        for error in e.errors:
+            logging.error("%s. Detail: %s", error.title, error.detail)
+        sys.exit(1)
+    except RequestError as e:
+        logging.error("%s - %s", e.error_code, e.message)
+        sys.exit(1)
+    except Exception as e:
+        logging.error("Unknown error occurred", exc_info=e)
+        sys.exit(1)
+
+
 if __name__ == "__main__":
-    cli()
+    main()

pyproject.toml
@@ -10,7 +10,7 @@ packages = [
 ]
 
 [tool.poetry.scripts]
-vilocify = 'vilocify.cli:cli'
+vilocify = 'vilocify.cli:main'
 
 [tool.poetry.dependencies]
 python = "^3.12"
@@ -23,6 +23,7 @@ pytest = "^8.1.1"
 ruff = "^0.9.7"
 mypy = "^1.11.2"
 types-requests = "^2.32.0.20240914"
+requests-mock = "^1.12.1"
 editorconfig-checker = "^3.2.1"
 
 [build-system]

tests/test_http.py
@@ -1,0 +1,52 @@
+#  SPDX-FileCopyrightText: 2025 Siemens AG
+#  SPDX-License-Identifier: MIT
+
+import pytest
+import requests_mock as rm
+
+from vilocify import http
+from vilocify.http import JSONAPIError, JSONAPIRequestError, RequestError
+
+
+def test_invalid_content_type(requests_mock: rm.Mocker):
+    url = "https://portal.vilocify.com/api/v2/componentRequests"
+    requests_mock.get(url, headers={"Content-Type": "application/json"})
+    with pytest.raises(RequestError) as e:
+        http.get(url)
+
+    assert e.value.message == "Unsupported content type in server response."
+
+
+def test_unauthorized_error(requests_mock: rm.Mocker):
+    url = "https://portal.vilocify.com/api/v2/componentRequests"
+    requests_mock.get(
+        url,
+        status_code=401,
+        headers={
+            "Content-Type": "application/vnd.api+json;"
+            ' profile="https://jsonapi.org/profiles/ethanresnick/cursor-pagination/"; charset=utf-8'
+        },
+        json={
+            "jsonapi": {
+                "version": "1.1",
+                "ext": [],
+                "profile": ["https://jsonapi.org/profiles/ethanresnick/cursor-pagination/"],
+            },
+            "errors": [
+                {
+                    "title": "Unauthorized - Missing Session or API Token",
+                    "status": "401",
+                    "detail": "Session and API Token are absent. Please log in or provide an API token.",
+                }
+            ],
+        },
+    )
+    with pytest.raises(JSONAPIRequestError) as e:
+        http.get(url)
+
+    assert len(e.value.errors) == 1
+    assert e.value.errors[0] == JSONAPIError(
+        title="Unauthorized - Missing Session or API Token",
+        detail="Session and API Token are absent. Please log in or provide an API token.",
+        source=None,
+    )

vilocify/http.py
@@ -2,6 +2,9 @@
 #  SPDX-License-Identifier: MIT
 
 import logging
+from dataclasses import dataclass
+
+import requests
 
 from vilocify import JSON, api_config
 
@@ -12,16 +15,48 @@ class RequestError(Exception):
         self.message = message
 
 
+@dataclass
+class JSONAPIError:
+    title: str = ""
+    detail: str = ""
+    source: dict | None = None
+
+    @staticmethod
+    def from_dict(d: dict) -> "JSONAPIError":
+        return JSONAPIError(d.get("title", ""), d.get("detail", ""), d.get("source"))
+
+
+class JSONAPIRequestError(RequestError):
+    def __init__(self, error_code: int, message: str, errors: list[JSONAPIError]):
+        super().__init__(error_code, message)
+        self.errors = errors
+
+    @staticmethod
+    def from_response(response: requests.Response) -> "JSONAPIRequestError":
+        errors = [JSONAPIError.from_dict(error) for error in response.json()["errors"]]
+        if errors:
+            message = f"Encountered errors: {', '.join(f"'{e.title}'" for e in errors)}"
+        else:
+            message = "Encountered unknown error. No error details were provided from the REST API."
+        return JSONAPIRequestError(response.status_code, message, errors)
+
+
 def _request(verb: str, url: str, json: JSON = None, params: dict[str, str] | None = None) -> JSON:
     logging.debug("%s: url=%s, params=%s, json=%s", verb.upper(), url, params, json)
     response = api_config.client.request(
         verb, url, timeout=api_config.request_timeout_seconds, json=json, params=params
     )
+    if not response.headers.get("Content-Type", "").startswith("application/vnd.api+json"):
+        raise RequestError(response.status_code, "Unsupported content type in server response.")
+
     if not response.ok:
-        raise RequestError(response.status_code, response.text)
+        raise JSONAPIRequestError.from_response(response)
+
     response_json = response.json() if response.text else None
     logging.debug("status_code=%s response=%s", response.status_code, response_json)
-    logging.debug("server-timing: %s", response.headers.get("Server-Timing", "n/a"))
+    if "Server-Timing" in response.headers:
+        logging.debug("server-timing: %s", response.headers["Server-Timing"])
+
     return response_json
 
 

commit e5c540bb46724bcd20d75c52ea789514156942b9

Fix missing --version option
1bd9485b77ead67a0ad0638f980c0f7eb789d4e4 accidentally removed the
version option.

Signed-off-by: Wolfgang Popp <popp.wolfgang@siemens.com>

vilocify/cli.py
@@ -15,6 +15,7 @@ from click import BadParameter
 from cyclonedx.model.bom import Bom
 from cyclonedx.model.bom import Component as BomComponent
 
+import vilocify
 from vilocify.http import JSONAPIRequestError, RequestError
 from vilocify.match import MissingPurlError, match_bom_component
 from vilocify.models import (
@@ -42,6 +43,7 @@ class BadCycloneDXFileError(Exception):
 
 @click.group()
 @click.option("--log-level", type=click.Choice(["DEBUG", "INFO", "ERROR"]), default="INFO")
+@click.version_option(version=vilocify.__version__, message=version_text)
 def cli(log_level: str):
     logging.basicConfig(format="%(levelname)s: %(message)s", level=log_level)
 