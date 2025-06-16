df4c8ffe884e5dc4a574f86ccd521a9662c7b52c
Wolfgang Popp <popp.wolfgang@siemens.com>
2025-06-16 12:27:19 +0200

    Add monitoringlist subcommands
    Add create and show sub-commands for monitoring lists.
    Also update and improve the help texts.
    
    ---------
    
    Signed-off-by: Wolfgang Popp <popp.wolfgang@siemens.com>

--- vilocify/cli.py
+++ vilocify/cli.py
@@ -3,6 +3,7 @@
 #  SPDX-FileCopyrightText: 2025 Siemens AG
 #  SPDX-License-Identifier: MIT
 
+import csv
 import io
 import json
 import logging
@@ -11,7 +12,8 @@ import textwrap
 from datetime import UTC, datetime, timedelta
 
 import click
-from click import BadParameter
+from click import BadParameter, UsageError
+from click.exceptions import Exit
 from cyclonedx.model.bom import Bom
 from cyclonedx.model.bom import Component as BomComponent
 
@@ -28,6 +30,7 @@ from vilocify.models import (
 
 ComponentCache = dict[tuple[str, str], Component]
 
+logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 version_text = """Vilocify Python SDK, version %(version)s
@@ -42,15 +45,27 @@ class BadCycloneDXFileError(Exception):
 
 
 @click.group()
-@click.option("--log-level", type=click.Choice(["DEBUG", "INFO", "ERROR"]), default="INFO")
+@click.option("--log-level", type=click.Choice(["DEBUG", "INFO", "ERROR"]), default="INFO", help="Set the log level.")
 @click.version_option(version=vilocify.__version__, message=version_text)
 def cli(log_level: str):
-    logging.basicConfig(format="%(levelname)s: %(message)s", level=log_level)
+    """To see help pages, you can run
+
+    \b
+    vilocify --help
+    vilocify <command> --help
+    vilocify <command> <subcommand> --help
+    """
+    logging.basicConfig(format="%(levelname)s: %(message)s", level=log_level, force=True)
 
 
 @cli.command()
-@click.option("--for", "monitoring_list", required=True)
-@click.option("--since", default=datetime.now(tz=UTC) - timedelta(days=1), type=click.DateTime())
+@click.option("--for", "monitoring_list", required=True, help="The monitoring list to print notifications for.")
+@click.option(
+    "--since",
+    default=datetime.now(tz=UTC) - timedelta(days=1),
+    type=click.DateTime(),
+    help="Print only notifications published after the given date.",
+)
 def notifications(monitoring_list: str, since: datetime):
     """Print all notifications for the given monitoring list since a certain date."""
     ns = Notification.where("monitoringLists.id", "any", monitoring_list).where("createdAt", "after", since.isoformat())
@@ -99,7 +114,7 @@ def _load_bom(file: io.FileIO) -> Bom:
     if len(bom.components) > MonitoringList.MAX_COMPONENTS:
         raise BadCycloneDXFileError(
             f"The CycloneDX file contains more than {MonitoringList.MAX_COMPONENTS} components, but Vilocify monitoring"
-            " lists cannot have more than 1000 components."
+            f" lists cannot have more than {MonitoringList.MAX_COMPONENTS} components."
         )
 
     return bom
@@ -134,12 +149,38 @@ def _match_bom(cache: ComponentCache, bom: Bom) -> tuple[list[Component], list[B
     return components, unidentified_components
 
 
-@cli.command()
-@click.option("--name", required=True)
-@click.option("--comment", default="")
-@click.option("--yes", is_flag=True)
-@click.option("--from-cyclonedx", type=click.File("rt"), required=True)
-def monitoringlist(name: str, comment: str, yes: bool, from_cyclonedx: io.FileIO):
+@cli.group()
+def monitoringlist():
+    """Manage monitoring lists."""
+
+
+@monitoringlist.command("show")
+@click.option(
+    "--format",
+    "export_format",
+    type=click.Choice(["extendedcsv"]),
+    default="extendedcsv",
+    help="The format used to print details.",
+)
+@click.option("--id", "monitoring_list_id", required=True, help="The monitoring list ID.")
+def monitoringlist_show(monitoring_list_id: str, export_format: str):
+    """Show monitoring list details."""
+    if export_format != "extendedcsv":
+        raise Exit(1)
+
+    ml = MonitoringList.get(monitoring_list_id)
+    csvwriter = csv.writer(sys.stdout)
+    csvwriter.writerow(["device", "vendor", "name", "version"])
+    for component in ml.components:
+        csvwriter.writerow([ml.name, component.vendor, component.name, component.version])
+
+
+@monitoringlist.command("import")
+@click.option("--name", required=True, help="The monitoring list name.")
+@click.option("--comment", default="", help="The comment set for the monitoring list.")
+@click.option("--yes", is_flag=True, help="Skip interactive questions. Assumes 'yes' for all answers.")
+@click.option("--from-cyclonedx", type=click.File("rt"), required=True, help="The CycloneDX file to import.")
+def monitoringlist_import(name: str, comment: str, yes: bool, from_cyclonedx: io.FileIO):
     """Creates or updates a monitoring list from a CycloneDX JSON or XML file.
 
     The monitoring list is identified by the given name and comment. Changing the name or comment between runs will
@@ -194,6 +235,7 @@ def monitoringlist(name: str, comment: str, yes: bool, from_cyclonedx: io.FileIO
     type=click.Choice(["unprocessed", "rejected", "mapped"]),
     multiple=True,
     default=["unprocessed", "rejected", "mapped"],
+    help="Filter component requests by state.",
 )
 def component_request(state: tuple[str]):
     """List component requests by processing state."""
@@ -218,6 +260,11 @@ def main():
         sys.exit(1)
     except (BadCycloneDXFileError, FileNotFoundError, BadParameter) as e:
         logger.error("%s", e)
+        sys.exit(1)
+    except UsageError as e:
+        logger.error("Bad command invocation")
+        print()
+        print(e.message)
         sys.exit(1)
     except Exception as e:
         logger.error("Unknown error occurred", exc_info=e)
